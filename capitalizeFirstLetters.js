077ff63fa60bbae6286bc21eb73bbc5389319d29
Thoms-C <61702462+Thoms-C@users.noreply.github.com>
2020-12-14 15:00:36 +0100

    rédac fonction

--- capitalizeFirstLetters.js
+++ capitalizeFirstLetters.js
@@ -1,5 +1,17 @@
 const assert = require("assert");
 
+function capitalizeFirstLetters(input) {
+    let output = '';
+    for (let i = 0 ; i < input.length ; i++) {
+      if (i === 0) {
+        output += input[0].toUpperCase();
+      } else {
+        output += input[i];
+      }
+    }
+    return output;
+  }
+
 assert.strictEqual(capitalizeFirstLetters.length, 1);
 
 assert.strictEqual(capitalizeFirstLetters('table'), 'Table')
